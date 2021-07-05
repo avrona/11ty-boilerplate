cc68050bd696383d4444fb8c804b6fed827a1485
avrona <alex@pointthegap.com>
2021-07-05 14:47:31 +0200

    Added standard config

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function (eleventyConfig) {
+
+
+    // Copy `img/` to `_site/img`
+  eleventyConfig.addPassthroughCopy("./src/images/");
+  eleventyConfig.addPassthroughCopy("./src/js/");
+  eleventyConfig.addPassthroughCopy("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/webfonts/");
+
+
+    return {
+markdownTemplateEngine: 'njk',
+dataTemplateEngine: 'njk',
+htmlTemplateEngine: 'njk',
+      dir: {
+        input: "src",
+        output: "public",
+      },
+    };
+  };
