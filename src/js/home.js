66891fda414724bd91a0098487410c5eb5b3d9f3
avrona <alex@pointthegap.com>
2021-07-14 22:27:09 +0200

    Fix FAQ icon toggle

--- src/js/home.js
+++ src/js/home.js
@@ -580,9 +580,13 @@ slideshow.addEventListener("touchmove", moveTouch, false);
 //------------------------------------------------------------------
 document.addEventListener('DOMContentLoaded', function() {
 	let cardHeaders = document.getElementsByClassName('card-header');
+  let cardIcons = document.getElementsByClassName('icon-toggle');
 	for (let i = 0; i < cardHeaders.length; i++) {
 		cardHeaders[i].addEventListener('click', e => {
 			e.currentTarget.parentElement.childNodes[3].classList.toggle('is-hidden');
+      cardIcons[i].classList.toggle('fa-angle-down');
+      cardIcons[i].classList.toggle('fa-angle-right');
+      //console.log(e.currentTarget.parentElement.childNodes);
 		});
 	}
 });
