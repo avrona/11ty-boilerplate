61fdb65265df3609aec776ac21c7baf39120acf7
avrona <alex@pointthegap.com>
2021-08-11 09:16:40 +0200

    Fix test.php link

--- src/js/contactform.js
+++ src/js/contactform.js
@@ -36,23 +36,57 @@ document.addEventListener('change', event => {
     }
   }, false)
 
-  document.addEventListener(
-    "click",
-    function(event) {
-      // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
-      if (
-        event.target.matches(".modal-close") ||
-        !event.target.closest(".modal")
-      ) {
-        closeModal()
-      }
-    },
-    false
-  )
-  
-  function closeModal() {
-    document.querySelector(".modal").style.display = "none"
+ document.addEventListener('DOMContentLoaded', function () {
+
+  // Modals
+
+  var rootEl = document.documentElement;
+  var $modals = getAll('.modal');
+  var $modalButtons = getAll('.modal-button');
+  var $modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
+
+  if ($modalButtons.length > 0) {
+    $modalButtons.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        rootEl.classList.add('is-clipped');
+        $target.classList.add('is-active');
+      });
+    });
   }
+
+  if ($modalCloses.length > 0) {
+    $modalCloses.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        closeModals();
+      });
+    });
+  }
+
+  document.addEventListener('keydown', function (event) {
+    var e = event || window.event;
+    if (e.keyCode === 27) {
+      closeModals();
+    }
+  });
+
+  function closeModals() {
+    rootEl.classList.remove('is-clipped');
+    $modals.forEach(function ($el) {
+      $el.classList.remove('is-active');
+    });
+  }
+
+  // Functions
+
+  function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+  }
+
+});
+  
+
 
 // First Name validation
   function validateFirstName() {
@@ -202,12 +236,13 @@ document.addEventListener('change', event => {
 
             
         // PHP script
-        xhr.open("POST", "test.php");
+        xhr.open("POST", "../test.php");
 
         xhr.onreadystatechange = function() { if (xhr.readyState === 4 && xhr.status === 200) { console.log(xhr.responseText);
-        successMessage.style.display = "block";
         } }
         xhr.setRequestHeader("Content-type", "application/json") // or "text/plain"
         xhr.send(new URLSearchParams(formData).toString()); 
 
 }
+
+
