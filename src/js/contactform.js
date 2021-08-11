1895013b598fc36c1f6953c1d95eb7c58c896565
avrona <alex@pointthegap.com>
2021-08-11 19:43:02 +0200

    Fix modal show and hide

--- src/js/contactform.js
+++ src/js/contactform.js
@@ -18,9 +18,9 @@ var emailValidated = false
 
 document.addEventListener("DOMContentLoaded", function() {
 emailMessage.style = 'display:none';
-successMessage.style = 'display:none';
+//successMessage.style = 'display:none';
 submit.disabled = true;
-submit.setAttribute("disabled","")
+submit.setAttribute("disabled","");
 }
 )
 
@@ -38,56 +38,7 @@ document.addEventListener('change', event => {
     }
   }, false)
 
- document.addEventListener('DOMContentLoaded', function () {
 
-  // Modals
-
-  var rootEl = document.documentElement;
-  var $modals = getAll('.modal');
-  var $modalButtons = getAll('.modal-button');
-  var $modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
-
-  if ($modalButtons.length > 0) {
-    $modalButtons.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-        rootEl.classList.add('is-clipped');
-        $target.classList.add('is-active');
-      });
-    });
-  }
-
-  if ($modalCloses.length > 0) {
-    $modalCloses.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-        closeModals();
-      });
-    });
-  }
-
-  document.addEventListener('keydown', function (event) {
-    var e = event || window.event;
-    if (e.keyCode === 27) {
-      closeModals();
-    }
-  });
-
-  function closeModals() {
-    rootEl.classList.remove('is-clipped');
-    $modals.forEach(function ($el) {
-      $el.classList.remove('is-active');
-    });
-  }
-
-  // Functions
-
-  function getAll(selector) {
-    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
-  }
-
-});
-  
 
 
 // First Name validation
@@ -219,9 +170,19 @@ document.addEventListener('change', event => {
   function sendForm () {
 
 
-    // Show the success message tab: 
+// Show the success message modal
+     
     
-
+    successMessage.classList.add('is-active');
+  
+    const exits = successMessage.querySelectorAll('.modal-exit');
+    exits.forEach(function(exit) {
+      exit.addEventListener('click', function(event) {
+        event.preventDefault();
+        successMessage.classList.remove('is-active');
+        window.location.reload();
+      });
+    });
 
 // Sending and receiving data in JSON format using POST method
         //
@@ -241,6 +202,19 @@ document.addEventListener('change', event => {
         xhr.open("POST", "../zformspost/contactform.php");
 
         xhr.onreadystatechange = function() { if (xhr.readyState === 4 && xhr.status === 200) { console.log(xhr.responseText);
+                                              // Show the success message modal
+                                        /* 
+                                        successMessage.classList.add('is-active');
+                                        const exits = successMessage.querySelectorAll('.modal-exit');
+                                        exits.forEach(function(exit) {
+                                          exit.addEventListener('click', function(event) {
+                                            event.preventDefault();
+                                            successMessage.classList.remove('is-active');
+                                            window.location.reload();
+                                          });
+                                        }); 
+                                        */
+
         } }
         // xhr.setRequestHeader("Content-type", "application/json") // or "text/plain"
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
