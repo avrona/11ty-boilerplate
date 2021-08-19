f374fe0a0b04f08b9d5d31aa9ada79c1ef83e52b
avrona <alex@pointthegap.com>
2021-08-19 23:53:49 +0200

    Fix revert back to easy schema JSON-LD

---
+++ src/_data/site.js
@@ -0,0 +1,12 @@
+
+   
+const baseUrl = "https://crm.6337.fr";
+
+module.exports = {
+    baseURL: `${baseUrl}`,
+    title: "6337.fr Réparation MacBook Pro & MacBook Air",
+    description: "Atelier de réparation experts en carte mères de MacBook, MacBook Pro et MacBook Air. 6337 répare des cartes HS, oxydées, des connecteurs arrachés...",
+    twitter: "6337Mac",
+    img: `${baseUrl}` + "/images/",
+    author: "6337 SAS"
+   };
