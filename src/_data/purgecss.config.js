b73b28018e22d5a543a2bda8e28a6460d7611dc0
avrona <alex@pointthegap.com>
2021-08-20 13:59:37 +0200

    Fix PurgeCCS config to keep navbar burger working

--- src/_data/purgecss.config.js
+++ src/_data/purgecss.config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
     // Content files referencing CSS classes
-    content: ["./dist/**/*.html"],
+    content: ["./dist/**/*.html", "./dist/**/*.js" ],
   
     // CSS files to be purged in-place
     css: ["./dist/**/*.css"],
