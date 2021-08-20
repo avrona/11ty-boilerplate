81351f5f600e385bd53b865fa290682291635e2d
avrona <alex@pointthegap.com>
2021-08-20 22:21:29 +0200

    Update purgecss.config.js
    Fix back to initial config, before whitelisting comment-sidecar

--- src/_data/purgecss.config.js
+++ src/_data/purgecss.config.js
@@ -6,21 +6,5 @@ module.exports = {
     content: ["./dist/**/*.html", "./dist/**/*.js" ],
   
     // CSS files to be purged in-place
-    // css: ["./dist/**/*.css"],
-    whitelistPatterns: [/^cs-/, /^comment-/], // Retain all classes starting with...
-    whitelist: [
-                'cs-title',
-                'cs-post',
-                'cs-avatar',
-                'cs-form-root',
-                'cs-body',
-                'cs-header',
-                'cs-date',
-                'cs-content',
-                'cs-form',
-                'cs-form-message',
-                'cs-url-group',
-                'cs-emailHint',
-               'comment-sidecar'
-               ]
+    css: ["./dist/**/*.css"],
   };
