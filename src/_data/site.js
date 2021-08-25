1376a4ec1990c2e6825d9a0bb3c4c8fb4efa097c
avrona <alex@pointthegap.com>
2021-08-25 15:09:57 +0200

    Fix opengraph image if loop for SEO

--- src/_data/site.js
+++ src/_data/site.js
@@ -8,5 +8,6 @@ module.exports = {
     description: "Atelier de réparation experts en carte mères de MacBook, MacBook Pro et MacBook Air. 6337 répare des cartes HS, oxydées, des connecteurs arrachés...",
     twitter: "6337Mac",
     img: `${baseUrl}` + "/images/",
+    header_image: `${baseUrl}` + "/images/MBP-export-800w.jpeg",
     author: "6337 SAS"
    };
