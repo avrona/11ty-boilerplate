e252639a8d9bc4b6d521a963b5aae28f43c7888a
avrona <avrona@users.noreply.github.com>
2021-09-14 20:50:15 +0000

    Deploying to master from @ avrona/6337-rawwebsite@4acdbb64b302ac19e1e0c2faf534c88269724a72 🚀

--- dist/js/navbarmanagement.js
+++ dist/js/navbarmanagement.js
@@ -30,30 +30,30 @@ document.addEventListener('DOMContentLoaded', () => {
 // ------------------  Google tag Manager ------------------------------
 //------------------------------------------------------------------
 
-document.addEventListener('DOMContentLoaded', () => {
-	/** init gtm after 3500 seconds - this could be adjusted */
-	setTimeout(initGTM, 3500);
-});
-document.addEventListener('scroll', initGTMOnEvent);
-document.addEventListener('mousemove', initGTMOnEvent);
-document.addEventListener('touchstart', initGTMOnEvent);
+// document.addEventListener('DOMContentLoaded', () => {
+// 	/** init gtm after 3500 seconds - this could be adjusted */
+// 	setTimeout(initGTM, 3500);
+// });
+// document.addEventListener('scroll', initGTMOnEvent);
+// document.addEventListener('mousemove', initGTMOnEvent);
+// document.addEventListener('touchstart', initGTMOnEvent);
 
-function initGTMOnEvent (event) {
-	initGTM();
-	event.currentTarget.removeEventListener(event.type, initGTMOnEvent); // remove the event listener that got triggered
-}
+// function initGTMOnEvent (event) {
+// 	initGTM();
+// 	event.currentTarget.removeEventListener(event.type, initGTMOnEvent); // remove the event listener that got triggered
+// }
 
-function initGTM () {
-	if (window.gtmDidInit) {
-		return false;
-	}
-	window.gtmDidInit = true; // flag to ensure script does not get added to DOM more than once.
-	const script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.async = true;
-	script.onload = () => { dataLayer.push({ event: 'gtm.js', 'gtm.start': (new Date()).getTime(), 'gtm.uniqueEventId': 0 }); } // this part ensures PageViews is always tracked
-	script.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-WQ8N9Z5';
+// function initGTM () {
+// 	if (window.gtmDidInit) {
+// 		return false;
+// 	}
+// 	window.gtmDidInit = true; // flag to ensure script does not get added to DOM more than once.
+// 	const script = document.createElement('script');
+// 	script.type = 'text/javascript';
+// 	script.async = true;
+// 	script.onload = () => { dataLayer.push({ event: 'gtm.js', 'gtm.start': (new Date()).getTime(), 'gtm.uniqueEventId': 0 }); } // this part ensures PageViews is always tracked
+// 	script.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-WQ8N9Z5';
 
-	document.head.appendChild(script);
-}
+// 	document.head.appendChild(script);
+// }
 
